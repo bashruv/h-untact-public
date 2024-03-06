cacc548c144edfebd9b4c526d0c9354015b2cf17
Dimi Lee <dimi@bashruv.dev>
2024-03-06 19:00:02 +0900

    컬러 시스템 추가

---
+++ app/archive/layout.tsx
@@ -0,0 +1,7 @@
+export default function ArchiveLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <main>{children}</main>;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,38 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      grayscale: {
+        "pure-white": "#FFFFFF",
+        white: "#EFEFEF",
+        gray: "#BABABA",
+        deep: "#636363",
+        dark: "#1E1E1E",
+        "pure-black": "#000000",
+      },
+      red: {
+        primary: "#FF0066",
+        dark: "#CC004D",
+        light: "#FF4C7F",
+      },
+      blue: {
+        primary: "#0033FF",
+        dark: "#001ACC",
+        light: "#0066FF",
+      },
+      yellow: {
+        primary: "#FFFF00",
+        dark: "#DEE132",
+        light: "#FFFF85",
+      },
+      green: {
+        primary: "#33FF99",
+        dark: "#2ED87B",
+        light: "#B5FEBD",
       },
     },
   },
