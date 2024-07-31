bf226889594a7f23145893d12ecc1bc57255cb89
Dimi L. <dimi@bashruv.dev>
2024-07-31 09:25:25 +0900

    chore: 버전 업데이트

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.scss",
   ],
   theme: {
     colors: {
@@ -35,8 +36,67 @@ const config: Config = {
         dark: "#2ED87B",
         light: "#B5FEBD",
       },
+      transparent: "rgba(0,0,0,0)",
+    },
+    fontSize: {
+      "10px": [
+        ".625rem",
+        {
+          letterSpacing: "-0.2px",
+          fontWeight: "500",
+        },
+      ],
+      "12px": [
+        ".75rem",
+        {
+          letterSpacing: "-0.24px",
+          fontWeight: "500",
+        },
+      ],
+      "14px": [
+        ".875rem",
+        {
+          letterSpacing: "-0.28px",
+          fontWeight: "500",
+        },
+      ],
+      "16px": [
+        "1rem",
+        {
+          letterSpacing: "-0.32px",
+          fontWeight: "500",
+        },
+      ],
+      "18px": [
+        "1.125rem",
+        {
+          letterSpacing: "-0.36px",
+          fontWeight: "500",
+        },
+      ],
+      "21px": [
+        "1.3125rem",
+        {
+          letterSpacing: "-0.42px",
+          fontWeight: "500",
+        },
+      ],
+      "24px": [
+        "1.5rem",
+        {
+          letterSpacing: "-0.48px",
+          fontWeight: "500",
+        },
+      ],
+      "30px": [
+        "1.875rem",
+        {
+          letterSpacing: "-0.6px",
+          fontWeight: "500",
+        },
+      ],
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")],
 };
 export default config;
